e702b7f085e590be8ba5448d7dc9226ad050bfc2
Angus <Angus@Lucifer.com>
2021-06-21 20:42:48 +0800

    fix:修复时间格式化问题

--- src/common/js/time-fmt.js
+++ src/common/js/time-fmt.js
@@ -19,17 +19,17 @@ export function timeRangeFmt(startTime, endTime, isFull = false) {
                 sameDay: "[今日] HH:mm",
                 nextDay: "[明日] HH:mm",
                 lastDay: "[昨日] HH:mm",
-                nextWeek: "MM月DD日 hh:mm",
-                lastWeek: "MM月DD日 hh:mm",
-                sameElse: "MM月DD日 hh:mm",
+                nextWeek: "MM月DD日 HH:mm",
+                lastWeek: "MM月DD日 HH:mm",
+                sameElse: "MM月DD日 HH:mm",
             }
             : {
                 sameDay: "YYYY-MM-DD([今日]) HH:mm",
                 nextDay: "YYYY-MM-DD([明日]) HH:mm",
                 lastDay: "YYYY-MM-DD([昨日]) HH:mm",
-                nextWeek: "YYYY-MM-DD(dddd) hh:mm",
-                lastWeek: "YYYY-MM-DD(dddd) hh:mm",
-                sameElse: "YYYY-MM-DD(dddd) hh:mm",
+                nextWeek: "YYYY-MM-DD(dddd) HH:mm",
+                lastWeek: "YYYY-MM-DD(dddd) HH:mm",
+                sameElse: "YYYY-MM-DD(dddd) HH:mm",
             }
     );
     const endTimeStr = $moment(endTime).calendar(
@@ -39,17 +39,17 @@ export function timeRangeFmt(startTime, endTime, isFull = false) {
                 sameDay: "HH:mm",
                 nextDay: "[次日] HH:mm",
                 lastDay: "[昨日] HH:mm",
-                nextWeek: "MM月DD日 hh:mm",
-                lastWeek: "MM月DD日 hh:mm",
-                sameElse: "MM月DD日 hh:mm",
+                nextWeek: "MM月DD日 HH:mm",
+                lastWeek: "MM月DD日 HH:mm",
+                sameElse: "MM月DD日 HH:mm",
             }
             : {
                 sameDay: "HH:mm",
                 nextDay: "[次日] HH:mm",
                 lastDay: "[昨日] HH:mm",
-                nextWeek: "YYYY-MM-DD(dddd) hh:mm",
-                lastWeek: "YYYY-MM-DD(dddd) hh:mm",
-                sameElse: "YYYY-MM-DD(dddd) hh:mm",
+                nextWeek: "YYYY-MM-DD(dddd) HH:mm",
+                lastWeek: "YYYY-MM-DD(dddd) HH:mm",
+                sameElse: "YYYY-MM-DD(dddd) HH:mm",
             }
     );
     return `${startTimeStr}${isFull ? "~" : "-"}${endTimeStr}`;
