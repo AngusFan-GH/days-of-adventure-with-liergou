f20c4b9db5931cf4c71d07e03ece321b7380e4cd
Angus <winterfyd@gmail.com>
2021-06-23 15:41:03 +0800

    feat: apiUrl区分环境

--- src/common/js/config.js
+++ src/common/js/config.js
@@ -1,3 +1,24 @@
 export const appId = 'wx62399ee33600accf';
 
-export const apiUrl = 'https://wx.heizhuidian.com';
+
+// 获取当前帐号信息
+const accountInfo = wx.getAccountInfoSync();
+
+// env类型
+export const env = accountInfo.miniProgram.envVersion;
+
+if (!env) {
+    console.error("获取运行环境失败!");
+}
+
+const baseApi = {
+    // 开发版
+    develop: "https://wx.heizhuidian.com",
+    // 体验版
+    trial: "https://wx.heizhuidian.com",
+    // 正式版
+    release: "https://wx.heizhuidian.com"
+};
+
+// request请求baseURL
+export const apiUrl = baseApi[env];
